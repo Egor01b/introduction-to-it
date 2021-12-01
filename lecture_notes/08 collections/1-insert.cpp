97afb6b6fe308dbbe003c1f7d24a2fc4bb4262b9
Egor Belous <belousegor5@gmail.com>
2021-12-01 10:08:51 +0200

    add 1-insert.cpp

---
+++ lecture_notes/08 collections/1-insert.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <cstdio>
+
+
+int main() {
+    std:: vector<int> ages = { 10, 12, 15, 15, 17, 18, 18, 19, 20 };
+    ages.pop_back();
+    ages.push_back(19);
+
+    std::vector <int>::iterator it;
+    it = ages.begin();
+
+    ages.insert(it,15);
+
+    ages.erase(it);
+
+
+
+    int first = ages[0];
+    int length = ages.size();
+    int last = ages[length - 1];
+
+
+    printf("first: %d\n", first);
+    printf("last: %d\n", last);
+}
