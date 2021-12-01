22dc58ebcc76f75d7a385aade83697f38d40e620
Egor Belous <belousegor5@gmail.com>
2021-12-01 10:16:17 +0200

    fix parametrs names in function.py

--- lecture_notes/08 collections/2-function.py
+++ lecture_notes/08 collections/2-function.py
@@ -1,11 +1,11 @@
 # Implementation
 
-def getFirstAndLast(array):
+def getFirstAndLast(ages):
   first = ages[0]
   last = ages[-1]
   return first, last
 
-# Usage
+# Usagex
 
 ages = [10, 12, 15, 15, 17, 18, 18, 19, 20]
 first, last = getFirstAndLast(ages)
