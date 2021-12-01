70466562b9f0f90539b97b0f95e354073cc78bb5
Egor Belous <belousegor5@gmail.com>
2021-12-01 11:37:29 +0200

    improved 0-buggy.js

--- lecture_notes/07 basics/0-buggy.js
+++ lecture_notes/07 basics/0-buggy.js
@@ -1,4 +1,14 @@
-let first_num = 2;
-let secord_num = 3;
-let sum = firstNum + secondNum;
-alert(sum);
+const firstNum = 2;
+const secondNum = 3;
+const sum = sumOfArgs(firstNum, secondNum);
+
+const sumOfArgs = (...args) => {
+    const sum = 0;
+    for (const i of args) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sum);
+
