aca8a74943a1ae5623f05d33dd81ad513152231e
Egor Belous <belousegor5@gmail.com>
2021-12-03 02:31:07 +0200

    delete empty lines

--- lecture_notes/08 collections/1-array.js
+++ lecture_notes/08 collections/1-array.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const ages = [10, 12, 15, 15, 17, 18, 18, 19, 20];
 
@@ -7,10 +7,8 @@ ages.push(12, 15, 17, 18, 19);
 ages.shift();
 ages.unshift(12, 15, 17, 18, 19);
 
-
-
-
 const first = ages[0];
 const last = ages[ages.length - 1];
 
 console.log({ first, last });
+
