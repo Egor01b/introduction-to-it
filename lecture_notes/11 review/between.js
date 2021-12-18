1caefa3b1ffca5d6b7b49a83c00d8b78d008dd6b
Egor Belous <belousegor5@gmail.com>
2021-12-18 13:30:23 +0200

    Fix code style in between.js

--- lecture_notes/11 review/between.js
+++ lecture_notes/11 review/between.js
@@ -1,18 +1,23 @@
+'use strict';
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p)
-    if ( i === -1 ) return ''
-    else {
-      k = i + p.length
-      str =  str.substring(k)
-      if ( s) {
-        i = str.indexOf( s )
-        if (i === -1) {
-          return  '' } else { str =  str.substring(0 , i)
-            }}}
-  return str
-}
+const getValueBetween = (str, p, s) => {
+  let i = str.indexOf(p);
+  if (i === -1) return '';
+  else {
+    const k = i + p.length;
+    str = str.substring(k);
+    if (s) {
+      i = str.indexOf(s);
+      if (i === -1) {
+        return '';
+      } else {
+        str = str.substring(0, i);
+      }
+    }
+  }
+  return str;
+};
 
-const result = getvaluebetween('Hello <username> and bye!', '<', '>')
-console.log(result)
+const result = getValueBetween('Hello <username> and bye!', '<', '>');
+console.log(result);
