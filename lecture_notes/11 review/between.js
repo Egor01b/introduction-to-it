411c7f545773b5351518010bc48d2fe782e09875
Egor Belous <belousegor5@gmail.com>
2021-12-19 18:18:06 +0200

    split responsibilities between functions

--- lecture_notes/11 review/between.js
+++ lecture_notes/11 review/between.js
@@ -1,20 +1,26 @@
 'use strict';
 // Extract substring between prefix and suffix
 
+const getValueAfter = (str, prefix) => {
+  const prefixStart = str.indexOf(prefix);
+  if (prefixStart === -1) return '';
+  const resultStart = prefixStart + prefix.length;
+  return str.substring(resultStart);
+}
+
+const getValueBefore = (str, suffix) => {
+  const suffixStart = str.indexOf(suffix);
+  if (suffixStart === -1) return '';
+  return str.substring(0, suffixStart);
+}
+
 const getValueBetween = (str, prefix, suffix) => {
-  let index = str.indexOf(prefix);
-  if (index === -1) return '';
-
-  const resultStart = index + prefix.length;
-  str = str.substring(resultStart);
+  str = getValueAfter(str, prefix);
   if (!suffix) return str;
-  
-  index = str.indexOf(suffix);
-  if (index === -1) return '';
-  str = str.substring(0, index);
-  return str;
-
-};
+  str = getValueBefore(str, suffix);
+  return str
+}
 
 const result = getValueBetween('Hello <username> and bye!', '<', '>');
 console.log(result);
+
